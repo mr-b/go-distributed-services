db1e6661644b54304701ad9d06a462aab386184d
Dave Brennan <dlbrennan@gmail.com>
2021-05-24 08:28:11 +0800

    3. Write log package

---
+++ internal/log/store_test.go
@@ -0,0 +1,30 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppenRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read__test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
